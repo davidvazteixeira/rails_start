6465720130881a51317414075ce5c5f5a669f553
David V.T <dvt4qb@gmail.com>
2014-09-03 16:37:48 -0300

    Migration to add name to Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828231804) do
+ActiveRecord::Schema.define(version: 20140903192823) do
 
   create_table "manufactures", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140828231804) do
     t.integer  "manufacture_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
