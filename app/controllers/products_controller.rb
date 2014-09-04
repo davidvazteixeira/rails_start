4b5e5a6014508cec2084bfc45b253762a27b8cea
davidvazteixeira <qbasicbr@gmail.com>
2014-09-04 19:49:55 -0300

    product controller refactor - part 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,12 +6,12 @@ class ProductsController < ApplicationController
 	end
 
 	def new
-		@manufacture = Manufacture.find(params[:manufacture_id])
+		manufacture
 		@product = Product.new
 	end
 
 	def create
-		@manufacture = Manufacture.find(params[:manufacture_id])
+		#@manufacture = Manufacture.find(params[:manufacture_id])
 
 		@product = Product.new
 		@product.name = params[:product][:name]
@@ -20,29 +20,34 @@ class ProductsController < ApplicationController
 		@product.manufacture_id = params[:manufacture_id]
 		@product.save
 
-		respond_with(@manufacture, @product)
+		respond_with(manufacture, @product)
 
 	end
 
 	def edit
-		@manufacture = Manufacture.find(params[:manufacture_id])
+		#@manufacture = Manufacture.find(params[:manufacture_id])]@manufacture = manufacture
+		manufacture
 		@product = Product.find(params[:id])
 	end
 
 	def update
-		@manufacture = Manufacture.find(params[:manufacture_id])
+		#@manufacture = Manufacture.find(params[:manufacture_id])
 		@product = Product.find(params[:id])
 		@product.name = params[:product][:name]
 		@product.engine = params[:product][:engine]
 		@product.tire = params[:product][:tire]
 		@product.save
 
-		respond_with(@manufacture, @product)
-
+		respond_with(manufacture, @product)
 	end
 
 	private
-    def create_params
+
+		def manufacture
+			@manufacture ||= Manufacture.find(params[:manufacture_id])
+		end
+
+		def create_params
 			params[:product].permit(:name, :engine, :tire)
 		end
 
