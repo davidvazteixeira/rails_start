afa2dfdc5f0dcb9f8401028e92dd2dc83711291f
David V.T <dvt4qb@gmail.com>
2014-09-16 13:38:56 -0300

    Fix user controller to add session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 	def create
 	  @user = User.new(create_params)
 	  if @user.save
+      session[:user_id] = @user.id
 			redirect_to manufactures_path, notice: "Account Created."
 		else
 			render 'new'
